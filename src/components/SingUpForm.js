de35fa331523d47d85b09e738e42f7f3e7f218a0
ali-khoshqamat <ali_khoshghamat@yahoo.com>
2022-12-01 03:14:42 +0330

    show the Error at the moment

--- src/components/SingUpForm.js
+++ src/components/SingUpForm.js
@@ -14,7 +14,6 @@ const onSubmit = (values) => {
   console.log(values);
 };
 const validate = (values) => {
-  //   console.log(values);
   let errors = {};
   !values.name && (errors.name = "Name is Required!");
   !values.email && (errors.email = "Eamil is Required!");
@@ -25,39 +24,63 @@ const validate = (values) => {
 const SingUpForm = () => {
   const formik = useFormik({ initialValues, onSubmit, validate });
 
-  console.log(formik.errors);
+  console.log("visited fields", formik.touched);
 
   return (
     <section className="w-[40rem] bg-[#24292f] text-white p-5 flex flex-col gap-y-5 rounded-lg">
       <h2 className="font-bold text-center">SingUp Form</h2>
       <form onSubmit={formik.handleSubmit} className="flex flex-col gap-y-2.5">
         <div className="flex flex-col">
-          <label className="text-sm">Name</label>
+          <div className="flex justify-between items-center mb-0.5">
+            <label className="text-sm">Name</label>
+            {formik.errors.name && formik.touched.name && (
+              <label className="text-sm text-red-600">
+                {formik.errors.name}
+              </label>
+            )}
+          </div>
           <input
             type="text"
             name="name"
             value={formik.values.name}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             className="py-1.5 px-2.5 text-[#24292f] border border-gray-300 rounded-md outline-none"
           />
         </div>
         <div className="flex flex-col">
-          <label className="text-sm">Email</label>
+          <div className="flex justify-between items-center mb-0.5">
+            <label className="text-sm">Eamil</label>
+            {formik.errors.email && formik.touched.email && (
+              <label className="text-sm text-red-600">
+                {formik.errors.email}
+              </label>
+            )}
+          </div>
           <input
             type="text"
             name="email"
             value={formik.values.email}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             className="py-1.5 px-2.5 text-[#24292f] border border-gray-300 rounded-md outline-none"
           />
         </div>
         <div className="flex flex-col">
-          <label className="text-sm">Password</label>
+          <div className="flex justify-between items-center mb-0.5">
+            <label className="text-sm">Password</label>
+            {formik.errors.password && formik.touched.password &&(
+              <label className="text-sm text-red-600">
+                {formik.errors.password}
+              </label>
+            )}
+          </div>
           <input
             type="text"
             name="password"
             value={formik.values.password}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             className="py-1.5 px-2.5 text-[#24292f] border border-gray-300 rounded-md outline-none"
           />
         </div>
